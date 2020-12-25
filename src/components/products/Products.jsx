5c9987ce3a178796bc3cd64a145d815997cb80f2
riwandi <riwandy10@gmail.com>
2020-12-25 14:44:21 +0700

    add styling in products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Grid from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import Product from './product/product'
 
 const products = [
--- src/components/products/product/product.jsx
+++ src/components/products/product/product.jsx
@@ -1,9 +1,11 @@
-import classes from '*.module.css'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
 import { AddShoppingCart } from '@material-ui/icons'
+import useStyles from './styles'
 
 // membuat layout spesifik untuk jenis produk
 function Product({product}) {
+  const classes = useStyles()
+
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image='' title={product.name} />
@@ -16,12 +18,14 @@ function Product({product}) {
             {product.price}
           </Typography>
         </div>
-        <Typography variant='h2' color='textSecondary' >
+        <Typography variant='body2' color='textSecondary' >
           {product.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-
+        <IconButton aria-label="Add to cart">
+          <AddShoppingCart />
+        </IconButton>
       </CardActions>
     </Card>
   )
